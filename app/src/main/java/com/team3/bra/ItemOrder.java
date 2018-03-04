2fbc812c86d7d0c5d8258f17c2771ee9f4335dea
condiom <123456*a>
2018-03-04 20:35:37 +0200

    Orders report created .csv file saves at sd/report/ folder

--- app/src/main/java/com/team3/bra/ItemOrder.java
+++ app/src/main/java/com/team3/bra/ItemOrder.java
@@ -17,10 +17,27 @@ public class ItemOrder implements Serializable{
     private boolean done;
     private String notes;
     private int itemOrderID;
-    private BigDecimal currentPrice;
+    private double currentPrice;
     private float vat;
 
-    public ItemOrder(Item item,int quantity,String notes) {
+    private String category;
+
+    @Override
+    public String toString() {
+        return "ItemOrder{" +
+                "itemID=" + itemID +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", quantity=" + quantity +
+                ", done=" + done +
+                ", notes='" + notes + '\'' +
+                ", itemOrderID=" + itemOrderID +
+                ", currentPrice=" + currentPrice +
+                ", vat=" + vat +
+                '}';
+    }
+
+    public ItemOrder(Item item, int quantity, String notes) {
         this.itemOrderID=-1;
         this.itemID = item.getId();
         this.name=item.getName();
@@ -31,25 +48,26 @@ public class ItemOrder implements Serializable{
 
     public ItemOrder(Vector<Object> vec) {
         this.itemID = (int) vec.get(0);
-        this.name=(String) vec.get(1).toString();
-        this.description=(String) vec.get(2).toString();
+        this.name= vec.get(1).toString();
+        this.description=vec.get(2).toString();
         this.quantity = (int) vec.get(3);
         this.done = (boolean) vec.get(4);
-        this.notes = (String) vec.get(5).toString();
+        this.notes = vec.get(5).toString();
         this.itemOrderID=(int) vec.get(6);
-        this.currentPrice = (BigDecimal) vec.get(7);
+        this.currentPrice = Double.parseDouble(vec.get(7).toString());
     }
 
     public ItemOrder(Vector<Object> vec,boolean alter) {
         this.itemID = (int) vec.get(0);
-        this.name=(String) vec.get(1).toString();
-        this.description=(String) vec.get(2).toString();
+        this.name= vec.get(1).toString();
+        this.description= vec.get(2).toString();
         this.quantity = (int) vec.get(3);
         this.done = (boolean) vec.get(4);
-        this.notes = (String) vec.get(5).toString();
+        this.notes =  vec.get(5).toString();
         this.itemOrderID=(int) vec.get(6);
-        this.currentPrice = (BigDecimal) vec.get(7);
+        this.currentPrice = Double.parseDouble(vec.get(7).toString());
         this.vat = Float.parseFloat(vec.get(8).toString());
+        this.category =vec.get(9).toString();
     }
 
 
@@ -93,4 +111,13 @@ public class ItemOrder implements Serializable{
     public void setNotes(String notes) {
         this.notes = notes;
     }
+
+    public double getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
 }
